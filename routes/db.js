127d0ccc2fdfd7f0a5bb75b7a44699ee177e5851
Louis Kinley <louis@atomicinfotech.com>
2014-08-20 13:26:24 -0700

    code cleanup

--- routes/db.js
+++ routes/db.js
@@ -1,12 +1,13 @@
-  mongo = require('mongodb').MongoClient;
-  mongo.connect('mongodb://127.0.0.1:24242/phlog_easy', function (err, db) {
-    if (err) {
-        throw err;
-    } else {
-      console.log("<--------------- MONGODB Listening --------------- >")
-      this.db = db;
-    }
-  }.bind(this));
+var mongo = require('mongodb').MongoClient;
+
+mongo.connect('mongodb://127.0.0.1:24242/phlog_easy', function (err, db) {
+  if (err) {
+      throw err;
+  } else {
+    console.log("<--------------- MONGODB Listening --------------- >")
+    this.db = db;
+  }
+}.bind(this));
 
 exports.work = function(req,res) {
 
@@ -16,19 +17,17 @@ exports.work = function(req,res) {
 
           this.collection.find().toArray(function(err, images) {
             if (!err) {
-              res.statusCode = 200;
-              res.setHeader('content-type', 'application/json');
-
+            
               if (images.length > 0) {
-                res.end(JSON.stringify({'images': images}));                
+                this.sendJSON(res, JSON.stringify({'images': images}));                
               } else {
                 this.readAndStore(pictureSet);
                 emitter.addListener('imagesStored', function(datas) {
                   if (datas) {
                     getImages();
                   } else 
-                  res.end('No images found in the DB or in public/images/' + pictureSet);
-                });
+                  this.sendJSON(res, 'No images found in the DB or in public/images/' + pictureSet);
+                }.bind(this));
               }
                 // res.end('No images found in that collection');
             } else {
@@ -79,3 +78,8 @@ exports.readAndStore = function(pictureSet) {
     }.bind(this))
 }.bind(this);
 
+this.sendJSON = function(res, data) {
+  res.statusCode = 200;
+  res.setHeader('content-type', 'application/json');
+  res.end(data);
+}
